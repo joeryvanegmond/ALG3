76463a85c1053c0b8eef47bbdedf2658f3f5d3c6
Joery van Egmond <joeryvanegmond@hotmail.com>
2019-09-24 21:09:53 +0200

    add practicum4

---
+++ Practicum4/Practicum4/NawDoublyLinkedList.cs
@@ -0,0 +1,37 @@
+using System;
+using Alg1.Practica.Utils;
+using Alg1.Practica.Utils.Models;
+
+namespace Alg1.Practica.Practicum4
+{
+    public class NawDoublyLinkedList
+    {
+        public DoubleLink First { get; set; }
+        public DoubleLink Last { get; set; }
+
+        public void InsertHead(NAW naw)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public NAW ItemAtIndex(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public DoubleLink SwapLinkWithNext(DoubleLink link)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void BubbleSort()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public BigO OrderOfBubbleSort()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Practicum4/Practicum4/UndoableNawArray.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Runtime.Remoting.Messaging;
+using Alg1.Practica.Practicum1;
+using Alg1.Practica.Utils.Models;
+
+namespace Alg1.Practica.Practicum4
+{
+    public class UndoableNawArray : NawArrayOrdered
+    {
+
+        public UndoLink First { get; private set; }
+        public UndoLink Current { get; private set; }
+
+        public UndoableNawArray(int size)
+            : base(size)
+        {
+            First = null;
+            Current = null;
+        }
+
+        public override void Add(NAW item)
+        {
+            base.Add(item);
+            AddOperation(Operation.Add, item);
+        }
+
+        private bool DoRemove(NAW naw)
+        {
+            var index = Find(naw);
+            if (index < 0)
+                return false;
+            RemoveAtIndex(index);
+            return true;
+        }
+
+        public override bool Remove(NAW item)
+        {
+            if (!DoRemove(item))
+                return false;
+            AddOperation(Operation.Remove, item);
+            return true;
+        }
+
+        public void Undo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Redo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void AddOperation(Operation operation, NAW naw)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void ApplyOperation(UndoLink link)
+        {
+            switch (link.Operation)
+            {
+                case Operation.Add:
+                    base.Add(link.Naw);
+                    break;
+
+                case Operation.Remove:
+                    DoRemove(link.Naw);
+                    break;
+            }
+        }
+
+        private void ReverseOperation(UndoLink link)
+        {
+            switch (link.Operation)
+            {
+                case Operation.Add:
+                    DoRemove(link.Naw);
+                    break;
+
+                case Operation.Remove:
+                    base.Add(link.Naw);
+                    break;
+            }
+        }
+    }
+}
