d5d2c56fde28e758a01f520fceba4f1724acb348
joeryvanegmond <joeryvanegmond@hotmail.com>
2019-09-26 23:18:57 +0200

    fix

--- Practicum4/Practicum4/NawDoublyLinkedList.cs
+++ Practicum4/Practicum4/NawDoublyLinkedList.cs
@@ -37,129 +37,42 @@ namespace Alg1.Practica.Practicum4
         {
             if (link != Last)
             {
-                var secondLink = link.Next;
+                DoubleLink secondLink = link.Next;
+                DoubleLink thirdLink = secondLink.Next;
+                DoubleLink pastLink = link.Previous;
 
                 if (link == First)
                 {
                     First = secondLink;
-                    secondLink.Next = link;
+                    link.Next = thirdLink;
                     link.Previous = secondLink;
-                    link.Next = link.Next.Next;
+                    secondLink.Next = link;
                     secondLink.Previous = null;
+                    thirdLink.Previous = link;
                 }
                 else if (secondLink == Last)
                 {
-                    //var laatste = Last;
-                    //        var eenNaLaatste = link;
-
-                    //        Last.Previous = laatste;
-                    //        laatste.Next = eenNaLaatste;
-                    //        laatste.Previous = link.Previous;
-                    //        link.Previous.Next = laatste;
-                    //        Last = eenNaLaatste;
-                    //        eenNaLaatste.Previous = laatste;
-                    //        Last.Next = null;
+                    Last = link;
+                    link.Next = null;
+                    link.Previous = secondLink;
+                    secondLink.Next = link;
+                    secondLink.Previous = pastLink;
+                    pastLink.Next = secondLink;
+                }
+                else
+                {
+                    thirdLink.Previous = link;
+                    link.Next = thirdLink;
+                    link.Previous = secondLink;
+                    secondLink.Next = link;
+                    secondLink.Previous = pastLink;
+                    pastLink.Next = secondLink;
                 }
                 return secondLink;
             }
             return null;
         }
-        //    if (link == Last) return null;
-
-        //    DoubleLink nextLink = link.Next;
-        //    DoubleLink secondNextLink = nextLink.Next;
-        //    DoubleLink prevLink = link.Previous;
-
-        //    if (link == First)
-        //    {
-        //        // swappen met eerste link uit array
-        //        First = nextLink;
-
-        //        link.Next = secondNextLink;
-        //        link.Previous = nextLink;
-
-        //        nextLink.Next = link;
-        //        nextLink.Previous = null;
-
-        //        secondNextLink.Previous = link;
-        //    }
-        //    else if (nextLink == Last)
-        //    {
-        //        // swappen met laatste link uit array
-        //        Last = link;
-
-        //        link.Next = null;
-        //        link.Previous = nextLink;
-
-        //        nextLink.Next = link;
-        //        nextLink.Previous = prevLink;
-
-        //        prevLink.Next = nextLink;
-        //    }
-        //    else
-        //    {
-        //        secondNextLink.Previous = link;
-
-        //        link.Next = secondNextLink;
-        //        link.Previous = nextLink;
-
-        //        nextLink.Next = link;
-        //        nextLink.Previous = prevLink;
-
-        //        prevLink.Next = nextLink;
-        //    }
-
-        //    return nextLink;
-        //}
-
-        //public DoubleLink SwapLinkWithNext(DoubleLink link)
-        //{
-        //    // swap first with second
-        //    if (link == First)
-        //    {
-        //        var eerste = link.Next;
-        //        var tweede = link;
-
-        //        First = eerste;
-        //        eerste.Next = tweede;
-        //        tweede.Previous = eerste;
-        //        tweede.Next = link.Next.Next;
-        //        eerste.Previous = null;
-        //    }
-        //    // swap second-last with last
-        //    else if (link.Next == Last)
-        //    {
-        //        var laatste = Last;
-        //        var eenNaLaatste = link;
-
-        //        Last.Previous = laatste;
-        //        laatste.Next = eenNaLaatste;
-        //        laatste.Previous = link.Previous;
-        //        link.Previous.Next = laatste;
-        //        Last = eenNaLaatste;
-        //        eenNaLaatste.Previous = laatste;
-        //        Last.Next = null;
-        //    }
-        //    // swap second with third
-        //    else if (link == First.Next)
-        //    {
-        //        var secondLink = link.Next;
-        //        //var tweede = link;
-        //        link.Next = secondLink.Next;
-        //        secondLink.Previous = link.Previous;
-        //        secondLink.Next = link;
-        //        link.Previous = secondLink;
-        //        //First.Next = derde;
-        //        //derde.Previous = First;
-        //        //derde.Next = tweede;
-        //        //tweede.Previous = derde;
-        //        //tweede.Next = Last;
-        //        //Last.Previous = tweede;
-        //    }
-        //    return link.Previous;
-        //}
-
-
+       
         public void BubbleSort()
         {
             throw new System.NotImplementedException();
