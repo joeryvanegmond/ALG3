9c1fcdab952c58639881cf09bc1b2aec4460d619
Joery van Egmond <joeryvanegmond@hotmail.com>
2019-10-06 21:21:32 +0200

    alles werkt

--- Practicum6/Practicum6/NawPriorityQueue.cs
+++ Practicum6/Practicum6/NawPriorityQueue.cs
@@ -1,24 +1,72 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using Alg1.Practica.Utils.Models;
 
 namespace Alg1.Practica.Practicum6
 {
     public class NawPriorityQueue
     {
+        private int _count;
+        private SortedList<int, Queue> pq = new SortedList<int, Queue>();
+        private int First { get; set; }
 
         public void Enqueue(int priority, NAW naw)
         {
-            throw new System.NotImplementedException();
+            Queue temp;
+
+            if (_count != 0)
+            {
+                if (pq.ContainsKey(priority))
+                {
+                    pq[priority].Enqueue(naw);
+                    
+                    _count++;
+                }
+                else 
+                {
+                    temp = new Queue();
+                    temp.Enqueue(naw);
+                    pq.Add(priority, temp);
+                    _count++;
+                }
+                First = pq.Keys[0];
+                return;
+            }
+
+            temp = new Queue();
+            temp.Enqueue(naw);
+            pq.Add(priority, temp);
+            First = pq.Keys[0];
+            _count++;
         }
 
         public NAW Dequeue()
         {
-            throw new System.NotImplementedException();
+            if (Count() != 0)
+            {
+                var temp = (NAW) pq[First].Dequeue();
+
+                if (pq[First].Count == 0)
+                {
+                    pq.RemoveAt(0);
+                }
+
+                _count--;
+
+                if (Count() != 0)
+                {
+                    First = pq.Keys[0];
+                }
+                
+                return temp;
+            }
+            return null;
         }
 
         public int Count()
         {
-            throw new System.NotImplementedException();
+            return _count;
         }
 
         public void Show()
--- Practicum6/Practicum6/Program.cs
+++ Practicum6/Practicum6/Program.cs
@@ -1,3 +1,4 @@
+using Alg1.Practica.Practicum6;
 using System;
 
 namespace Practicum6
@@ -7,6 +8,39 @@ namespace Practicum6
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            NawPriorityQueue pq = new NawPriorityQueue();
+
+            //pq.Enqueue(4, new Alg1.Practica.Utils.Models.NAW("2", "1", "1"));
+            //pq.Enqueue(5, new Alg1.Practica.Utils.Models.NAW("1", "1", "1"));
+            //pq.Enqueue(3, new Alg1.Practica.Utils.Models.NAW("9", "3", "3"));
+            //pq.Enqueue(9, new Alg1.Practica.Utils.Models.NAW("8", "3", "3"));
+            //pq.Enqueue(3, new Alg1.Practica.Utils.Models.NAW("7", "3", "3"));
+            //pq.Enqueue(1, new Alg1.Practica.Utils.Models.NAW("6", "3", "3"));
+            //pq.Enqueue(6, new Alg1.Practica.Utils.Models.NAW("5", "3", "3"));
+            //pq.Enqueue(3, new Alg1.Practica.Utils.Models.NAW("4", "3", "3"));
+            //pq.Enqueue(2, new Alg1.Practica.Utils.Models.NAW("3", "3", "3"));
+            //pq.Enqueue(1, new Alg1.Practica.Utils.Models.NAW("4", "3", "3"));
+            //pq.Enqueue(1, new Alg1.Practica.Utils.Models.NAW("3", "3", "3"));
+            //pq.Enqueue(1, new Alg1.Practica.Utils.Models.NAW("2", "2", "2"));
+            //pq.Enqueue(1, new Alg1.Practica.Utils.Models.NAW("1", "2", "2"));
+            //pq.Enqueue(2, new Alg1.Practica.Utils.Models.NAW("2", "2", "2"));
+            //pq.Enqueue(2, new Alg1.Practica.Utils.Models.NAW("6", "6", "6"));
+            //pq.Enqueue(2, new Alg1.Practica.Utils.Models.NAW("2", "2", "2"));
+            //Console.WriteLine(pq.Peek());
+
+            Console.WriteLine(pq.Dequeue());
+            //Console.WriteLine(pq.Dequeue());
+            //Console.WriteLine(pq.Dequeue());
+            //Console.WriteLine(pq.Dequeue());
+            //Console.WriteLine(pq.Dequeue());
+            //Console.WriteLine(pq.Dequeue());
+            //Console.WriteLine(pq.Dequeue());
+            //Console.WriteLine(pq.Dequeue());
+            //Console.WriteLine(pq.Dequeue());
+            //Console.WriteLine(pq.Dequeue());
+            //Console.WriteLine(pq.Dequeue());
+            pq.Show();
         }
     }
 }
